867d9b9494219af1020be9e7c2c9c45c7fdd3d10
K8504 <K8504@users.noreply.github.com>
2017-02-02 02:22:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TV tv = new TV();
+            tv.IsOn = true;
+            tv.Channel = 6;
+            tv.Volume = 15;
+            Console.WriteLine("TV is turned On");
+            Console.WriteLine("Loading...");
+            Console.WriteLine("Channel has been switched to " + tv.Channel);
+            Console.WriteLine("Volume: " + tv.Volume);
+            
+
+
+        }
+    }
+}
